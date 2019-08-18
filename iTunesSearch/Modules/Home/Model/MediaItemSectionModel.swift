8c1fb26e6bab7c8ef7c959782fe7322d2f975a2e
Ron Daryl Magno <rmagno@yondu.com>
2019-08-18 09:44:42 +0800

    Add section model representation of MediaItem

---
+++ iTunesSearch/Modules/Home/Model/MediaItemSectionModel.swift
@@ -0,0 +1,26 @@
+//
+//  MediaItemSectionModel.swift
+//  iTunesSearch
+//
+//  Created by Ron Daryl Magno on 17/08/2019.
+//  Copyright © 2019 Ron Daryl Magno. All rights reserved.
+//
+
+import Foundation
+import RxDataSources
+
+struct MediaItemSectionModel {
+    var items: [MediaItem]
+
+    init(items: [MediaItem]) {
+        self.items = items
+    }
+}
+
+extension MediaItemSectionModel: SectionModelType {
+    typealias Item = MediaItem
+
+    init(original: MediaItemSectionModel, items: [MediaItem]) {
+        self = MediaItemSectionModel(items: items)
+    }
+}
