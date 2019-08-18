3caaddb197ccf8ef8f1852392f8d5192b106c5b9
Ron Daryl Magno <rmagno@yondu.com>
2019-08-18 09:39:48 +0800

    Add view setup for tableviw

--- iTunesSearch/Modules/Home/HomeViewController.swift
+++ iTunesSearch/Modules/Home/HomeViewController.swift
@@ -9,20 +9,40 @@
 import UIKit
 import RxSwift
 import RxDataSources
+import SnapKit
 
 class HomeViewController: UIViewController, NibInstantiated {
     private let disposeBag = DisposeBag()
 
     private let searchBar = UISearchBar()
-    @IBOutlet var searchResultsTableView: UITableView!
+    @IBOutlet private var searchResultsTableView: UITableView!
+    private let loadingIndicator = UIActivityIndicatorView()
 
     var viewModel: HomeViewModel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.titleView = searchBar
+        setupView()
         setupInputBinding()
         setupOutputBinding()
+        setupTableView()
+    }
+
+    private func setupView() {
+        view.addSubview(loadingIndicator)
+        loadingIndicator.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        loadingIndicator.color = .gray
+
+        searchBar.autocorrectionType = .no
+        searchBar.autocapitalizationType = .none
+    }
+
+    private func setupTableView() {
+        searchResultsTableView.rowHeight = UITableView.automaticDimension
+        searchResultsTableView.register(nib: MediaItemTableViewCell.self)
     }
 }
 
