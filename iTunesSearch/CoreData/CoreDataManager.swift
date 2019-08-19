39003020ca43a4da56100f26447104b039b78c88
Ron Daryl Magno <rmagno@yondu.com>
2019-08-19 16:29:42 +0800

    Add coredata stack

---
+++ iTunesSearch/CoreData/CoreDataManager.swift
@@ -0,0 +1,40 @@
+//
+//  CoreDataManager.swift
+//  iTunesSearch
+//
+//  Created by Ron Daryl Magno on 19/08/2019.
+//  Copyright © 2019 Ron Daryl Magno. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+// MARK: - Core Data stack
+class CoreDataManager {
+    static let shared: CoreDataManager = CoreDataManager()
+
+    private init() { }
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "iTunesSearch")
+        container.loadPersistentStores(completionHandler: { (_, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
