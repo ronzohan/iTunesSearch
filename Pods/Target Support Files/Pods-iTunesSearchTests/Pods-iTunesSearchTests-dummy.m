e474ca5ce233f4549925ffde3abbbdca78ca3d90
Ron Daryl Magno <ron.zohan@gmail.com>
2019-08-21 13:19:30 +0800

    Add pod files

---
+++ Pods/Target Support Files/Pods-iTunesSearchTests/Pods-iTunesSearchTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iTunesSearchTests : NSObject
+@end
+@implementation PodsDummy_Pods_iTunesSearchTests
+@end
---
+++ Pods/Target Support Files/Pods-iTunesSearchUITests/Pods-iTunesSearchUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iTunesSearchUITests : NSObject
+@end
+@implementation PodsDummy_Pods_iTunesSearchUITests
+@end
