cefba7eeb79908b095e5f9b37fe16137f4226386
Ron Daryl Magno <ron.zohan@gmail.com>
2019-08-21 10:52:09 +0800

    Remove labels and instead use button

--- iTunesSearch/Modules/Home/Cells/MediaItemTableViewCell.swift
+++ iTunesSearch/Modules/Home/Cells/MediaItemTableViewCell.swift
@@ -11,24 +11,23 @@ import Kingfisher
 
 class MediaItemTableViewCell: UITableViewCell {
     @IBOutlet var titleLabel: UILabel!
-    @IBOutlet var priceLabel: UILabel!
     @IBOutlet var castLabel: UILabel!
     @IBOutlet var genreLabel: UILabel!
     @IBOutlet var artworkImageView: UIImageView!
-    @IBOutlet var rentalPrice: UILabel!
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        artworkImageView.layer.cornerRadius = 10
-        artworkImageView.clipsToBounds = true
-    }
+    @IBOutlet var priceButton: UIButton!
+    @IBOutlet var rentButton: UIButton!
 
     func configure(with mediaItemViewModel: MediaItemViewModel) {
         titleLabel.text = mediaItemViewModel.trackName
-        priceLabel.text = mediaItemViewModel.buyPrice
+        priceButton.setTitle(mediaItemViewModel.buyPrice, for: .normal)
+        priceButton.isHidden = mediaItemViewModel.buyPrice == nil
+
+        rentButton.setTitle(mediaItemViewModel.rentPrice, for: .normal)
+        rentButton.isHidden = mediaItemViewModel.rentPrice == nil
+
         castLabel.text = mediaItemViewModel.artistName
         genreLabel.text = mediaItemViewModel.genre
-        rentalPrice.text = mediaItemViewModel.rentPrice
+
         if let url = mediaItemViewModel.imageURL {
             artworkImageView.kf.setImage(with: url)
         }
